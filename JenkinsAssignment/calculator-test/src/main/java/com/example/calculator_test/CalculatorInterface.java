65b5137a327429db794c51b439078958d8c3ef45
araowis <aditi.rao@wissen.com>
2026-02-25 17:53:52 +0530

    add: Docker assignment and removed: jenkins assignment

--- JenkinsAssignment/calculator-test/src/main/java/com/example/calculator_test/CalculatorInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.calculator_test;
-
-public interface CalculatorInterface {
-    public int add(int a, int b);
-    public int divide(int a, int b);
-    public int mul(int a, int b);
-    public int subt(int a, int b);
-    public void logSomethingNTimes();
-}
